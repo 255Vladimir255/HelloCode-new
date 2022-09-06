759040c984ffadf15ca556797f76c6f62da30e9a
Vladimir <shved255@yandex.ru>
2022-09-06 18:05:14 +0300

    new repozit

---
+++ L1/Examp002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine ("Введите ваше имя");
+string username = Console.ReadLine ();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+Console.Write("Привет, "); // выводит в одну строку
+Console.Write(username); // выводит в одну строку
---
+++ S1/task001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два 
+//числа и проверяет, является ли первое число квадратом второго.
+//например:
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3, b = 9 -> нет
+
+Console.WriteLine("введите первое число");
+double num1 = Convert.ToDouble(Console.ReadLine());   //Convert.ToInt32() (преобразует к типу int), Convert.ToDouble() (преобразует к типу double),
+                                                      //Convert.ToDecimal() (преобразует к типу decimal)
+Console.WriteLine("введите второе число");
+double num2 = Convert.ToDouble(Console.ReadLine());
+double result = Math.Sqrt(num1);
+if (result == +- num2) // ковырялся забыл поставить значение равенства ==, вместо = присвоить)))
+{
+Console.Write("первое число  является квадратом второго ");
+}
+    else
+    {
+Console.Write("первое число  не является квадратом второго ");
+    }

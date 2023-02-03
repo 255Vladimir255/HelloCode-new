87cf0b1c985964e9634dfb16d0cff0b4e466a397
255Vladimir255 <102412085+255Vladimir255@users.noreply.github.com>
2023-02-03 14:49:34 +0200

    CreatHM

--- S5/HomeWork36/Program.cs
+++ S5/HomeWork36/Program.cs
@@ -2,3 +2,27 @@
 //[3, 7, 23, 12] -> 19
 //[-4, -6, 89, 6] -> 0
 
+int RandomNumbers(int NumbersArray, int min, int max)
+  {
+  int[] randomNumber = new int[NumbersArray];
+  int sumElements = 0;
+  Console.Write("Получившийся массив: ");
+
+    for (int i = 0; i <randomNumber.Length; i++ ){
+      randomNumber[i] = new Random().Next(min, max);
+
+      Console.Write(randomNumber[i] + " ");
+
+      if (i % 2 == 0)
+      {
+        sumElements = sumElements + randomNumber[i];
+      }
+    }
+  return sumElements;
+  }
+
+
+Console.WriteLine($"Введи длину массива: ");
+int NumbersArray = Convert.ToInt32(Console.ReadLine()); 
+int randomNumber =  RandomNumbers(NumbersArray, 1, 10);
+Console.WriteLine($"  Сумма элементов, стоящих на чётных позициях: {randomNumber}");
